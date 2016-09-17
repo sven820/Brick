307762022c91b9000e921effe85dd65454c4d365
jinxiaofei <jinxiaofei@yaochufa.com>
2016-09-17 14:51:14 +0800

    重置

--- BrickKit/Pods/Target Support Files/Pods-BrickKit-BrickKitTests/Pods-BrickKit-BrickKitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BrickKit_BrickKitTests : NSObject
-@end
-@implementation PodsDummy_Pods_BrickKit_BrickKitTests
-@end
--- BrickKit/Pods/Target Support Files/Pods-BrickKit-BrickKitTests/Pods-BrickKit-BrickKitTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_BrickKit_BrickKitTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BrickKit_BrickKitTestsVersionString[];
-
