bf9b9f16a430e9d5fca5666bf10984cfb58e22bb
jinxiaofei <jinxiaofei@yaochufa.com>
2016-09-17 16:57:40 +0800

    修改safe 集合安全类API

--- Brick/Brick/BrickKit/Tools/EvadeCrash/NSString+YXYSafe.h
+++ Brick/Brick/BrickKit/Tools/EvadeCrash/NSString+YXYSafe.h
@@ -2,13 +2,18 @@
 //  NSString+YCFSafe.h
 //  YCFComponentKit_OC
 //
-//  Created by 林小程 on 16/8/9.
+//  Created by JJ.sven on 16/8/9.
 //  Copyright © 2016年 yaochufa. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface NSString (YCFSafe)
+@interface NSString (YXYSafe)
+
+
+#pragma mark - 仿止crash容错API
+//检查字符串是否为空
++ (BOOL)checkIsEmptyOrNull:(NSString *) string;
 
 /**
  *  [NSString rangeOfString]的容错API，当源字符串或者要查找的字符串为空时，返回NotFound
@@ -28,8 +33,16 @@
  */
 + (NSRange)safe_rangeOfCharacterFromSet:(NSCharacterSet *)searchSet inSourceString:(NSString *)source;
 
+//返回安全的字符串，不会返回nil，至少返回空字符串，主要使用场景：使用字面量初始化字典时
++ (NSString *)safe_string:(NSString *)aString;
 
-//检查字符串是否为空
-+ (BOOL)checkIsEmptyOrNull:(NSString *) string;
+/**
+ *  安全拼接字符串,解决使用NSString的stringByAppendingString:方法不小心传入空参数导致crash的问题
+ *
+ *  @param aString 要拼接到原字符串后面的字符串
+ *
+ *  @return 若aString为null字符串或者nil则返回self，否则返回拼接后的字符串
+ */
+- (NSString *)safe_stringByAppendingString:(NSString *)aString;
 
 @end
--- Brick/Brick/BrickKit/Tools/EvadeCrash/NSString+YXYSafe.m
+++ Brick/Brick/BrickKit/Tools/EvadeCrash/NSString+YXYSafe.m
@@ -2,13 +2,21 @@
 //  NSString+YCFSafe.m
 //  YCFComponentKit_OC
 //
-//  Created by 林小程 on 16/8/9.
+//  Created by JJ.sven on 16/8/9.
 //  Copyright © 2016年 yaochufa. All rights reserved.
 //
 
-#import "NSString+YCFSafe.h"
+#import "NSString+YXYSafe.h"
 
-@implementation NSString (YCFSafe)
+@implementation NSString (YXYSafe)
++ (BOOL)checkIsEmptyOrNull:(NSString *)string
+{
+    if (string && ![string isEqual:[NSNull null]] && ![string isEqual:@"(null)"] && ![string isEqualToString:@"<null>"] && [string length] > 0)
+    {
+        return NO;
+    }
+    return YES;
+}
 
 + (NSRange)safe_rangeOfString:(NSString *)searchString inSourceString:(NSString *)source
 {
@@ -36,13 +44,28 @@
     }
 }
 
-
-+ (BOOL)checkIsEmptyOrNull:(NSString *) string
++ (NSString *)safe_string:(NSString *)aString
 {
-    if (string && ![string isEqual:[NSNull null]] && ![string isEqual:@"(null)"] && ![string isEqualToString:@"<null>"] && [string length] > 0)
+    if ([NSString checkIsEmptyOrNull:aString])
     {
-        return NO;
+        return @"";
     }
-    return YES;
+    else
+    {
+        return aString;
+    }
 }
+
+- (NSString *)safe_stringByAppendingString:(NSString *)aString
+{
+    if ([NSString checkIsEmptyOrNull:aString])
+    {
+        return self;
+    }
+    else
+    {
+        return [self stringByAppendingString:aString];
+    }
+}
+
 @end
